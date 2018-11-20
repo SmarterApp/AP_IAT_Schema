d646551757d4d3277b86bdc7aabbbf475d4ab59b
Brett Drainer <bdrainer@fairwaytech.com>
2018-11-19 16:07:36 -0800

    IAT-2583:  Renamed tables, columns, etc. to match what came out of the design meeting

--- src/main/resources/db/migration/V4__iat_ddl.sql
+++ src/main/resources/db/migration/V4__iat_ddl.sql
@@ -1,18 +1,18 @@
 
 -- starting sequence
-CREATE SEQUENCE IF NOT EXISTS bulk_item_job_seq START 1;
+CREATE SEQUENCE IF NOT EXISTS item_bulk_update_seq START 1;
 
-CREATE TABLE IF NOT EXISTS bulk_item_job (
-  id                UUID        NOT NULL,
-  job_id            TEXT        NOT NULL CHECK (char_length(job_type)   <= 255),
-  job_type          TEXT        NOT NULL CHECK (char_length(job_type)   <= 255),
-  job_status        TEXT        NOT NULL CHECK (char_length(job_status) <= 255),
-  job_json          JSON,
-  is_active         BOOLEAN     NOT NULL DEFAULT TRUE,
-  created_date      TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
-  created_by        VARCHAR     NOT NULL,
-  CONSTRAINT pk_bulk_item_job PRIMARY KEY (id),
-  CONSTRAINT uk_bulk_item_job UNIQUE (job_id, job_type, created_date)
+CREATE TABLE IF NOT EXISTS item_bulk_update (
+  id                    UUID        NOT NULL,
+  item_bulk_update_id   TEXT        NOT NULL CHECK (char_length(item_bulk_update_id)  <= 255),
+  update_type           TEXT        NOT NULL CHECK (char_length(update_type)          <= 255),
+  item_bulk_update_json JSON,
+  created_date          TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  created_by            TEXT        NOT NULL CHECK (char_length(created_by)           <= 255),
+  updated_date          TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+  updated_by            TEXT        NOT NULL CHECK (char_length(updated_by)           <= 255),
+  CONSTRAINT pk_item_bulk_update PRIMARY KEY (id),
+  CONSTRAINT uk_item_bulk_update UNIQUE (item_bulk_update_id, created_date)
 );
 
 
