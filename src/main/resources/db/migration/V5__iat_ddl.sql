0590451c3f3f25c46b0789713a50828cb4f5fb30
Ray Mitchell <ray@raymondmitchell.com>
2019-01-09 16:50:45 -0800

    IAT-2767: Change the transition (TTS Upload --> Accessibility Upload) to (TTS Upload --> TTS Review).

---
+++ src/main/resources/db/migration/V5__iat_ddl.sql
@@ -0,0 +1,25 @@
+-- https://jira.fairwaytech.com/browse/IAT-2767
+-- Change the transition:  TTS Upload --> Accessibility Upload
+-- to:                     TTS Upload --> TTS Review
+
+-- Select row to be updated (expected = 1 row)
+-- select * from workflow_status_transition
+-- where from_code = 'TextToSpeechUpload'
+-- 	and to_code = 'AccessibilityUpload'
+
+-- Up
+update workflow_status_transition
+set to_code = 'TextToSpeechReview'
+where from_code = 'TextToSpeechUpload'
+	and to_code = 'AccessibilityUpload'
+
+-- Select row that was updated (expected = 1 row)
+-- select * from workflow_status_transition
+-- where from_code = 'TextToSpeechUpload'
+-- 	and to_code = 'TextToSpeechReview'
+
+-- Down
+--update workflow_status_transition
+--set to_code = 'AccessibilityUpload'
+--where from_code = 'TextToSpeechUpload'
+--	and to_code = 'TextToSpeechReview'
