e69cb968c6196a7cf480947ec3868f9a448e5e7f
Brett Drainer <bdrainer@fairwaytech.com>
2019-07-02 14:39:34 -0700

    IAT-3155: Final pass at the new ITEM_HISTORY table

--- src/main/resources/db/migration/V12__iat_ddl.sql
+++ src/main/resources/db/migration/V12__iat_ddl.sql
@@ -5,25 +5,19 @@
 -- item history
 CREATE TABLE IF NOT EXISTS item_history (
   id                    UUID        NOT NULL,
-
   item_id               TEXT        NOT NULL CHECK (char_length(item_id)  <= 255),
-
   item_commit_id        UUID,
   branch_commit_id      UUID,
-
   content_version_id    TEXT        CHECK (char_length(content_version_id)  <= 255),
-
-  commit_by             VARCHAR     NOT NULL,
+  commit_by             TEXT        CHECK (char_length(content_version_id)  <= 255),
   commit_date           TIMESTAMPTZ NOT NULL,
   commit_message        TEXT        NOT NULL,
-
   created_date          TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
-
   CONSTRAINT pk_item_history PRIMARY KEY (id),
   UNIQUE (item_id, item_commit_id, commit_date)
 );
 
--- flywayClean deletes all user privileges, so we will set them here
+-- Adding a table requires these statements to be run
 -- Assumes that iat user has already been created in the database
 GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON ALL TABLES in schema public to "iat";
 GRANT SELECT, UPDATE ON ALL SEQUENCES in schema public to "iat";
